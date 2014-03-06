d9e0552d148d5824187a1762e734938c70d4c03d
Euan Goddard <euangoddard@2degreesnetwork.com>
2014-03-06 17:04:04 +0000

    Added a wrapper around the output

--- clipboard2markdown.js
+++ clipboard2markdown.js
@@ -4,13 +4,14 @@
     document.addEventListener('DOMContentLoaded', function () {
         var paste_bin = document.querySelector('#paste-bin');
         var output = document.querySelector('#output');
+        var output_wrapper = document.querySelector('#output-wrapper');
 
         document.addEventListener('keydown', function () {
             if (event.ctrlKey || event.metaKey) {
                 if (String.fromCharCode(event.which).toLowerCase() === 'v') {
                     paste_bin.innerHTML = '';
                     paste_bin.focus();
-                    output.classList.add('hidden');
+                    output_wrapper.classList.add('hidden');
                 }
             }
         });
@@ -20,11 +21,12 @@
         });
 
         var read_paste_bin = function () {
-            var html = paste_bin.innerHTML;
-            output.classList.remove('hidden');
-            output.textContent = html2markdown(html, {
+            var text_html = paste_bin.innerHTML;
+            var text_markdown = html2markdown(text_html, {
                 inlineStyle: true
             });
+            output.textContent = text_markdown;
+            output_wrapper.classList.remove('hidden');
         };
 
     });
