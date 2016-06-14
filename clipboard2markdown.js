3ccc80614902bc6e44d767d00bf74fa5e9c399e6
Vegard Øye <vegard_oye@hotmail.com>
2016-06-14 22:36:31 +0200

    Delete trailing whitespace

--- clipboard2markdown.js
+++ clipboard2markdown.js
@@ -12,6 +12,7 @@ function asciify(str) {
               .replace(/\n-\n/g, "\n")
               .replace(/\n\n\s*\\\n/g, "\n\n")
               .replace(/\n\n\n*/g, "\n\n")
+              .replace(/[ ]+$/gm, "")
               .replace(/^\s+|[\s\\]+$/g, "");
 }
 
